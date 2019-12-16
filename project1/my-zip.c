792b8bf372cc52cfc8e24664ec23691cea9ce840
HugoHutri <Hugo.Hutri@student.lut.fi>
2019-12-16 15:02:55 +0200

    add readme

--- project1/my-zip.c
+++ project1/my-zip.c
@@ -4,34 +4,41 @@
 int main(int argc, char *argv[]) {
     if(argc < 2) printf("no input files\n");
 
-    FILE *fp = fopen(argv[1],"r");
-
-    char c;
-    char prev;
-    int count = 0;
-
-    while(1) {
-        char c = fgetc(fp);
-
-        if(feof(fp)) {
-            fwrite(&count, sizeof count, 1, stdout);
-            fwrite(&prev, 1, 1, stdout);
-            break;
-        }
-        
-        else if(c == prev) {
-            prev = c;
-            count++;
+    for(int i = 1; i < argc; i++)
+    {
+        FILE *fp = fopen(argv[i],"r");
+        if(fp == NULL) {
+            printf("Tiedoston %s avaaminen epäonnistui\n", argv[i]);
             continue;
         }
 
-        else if(count != 0) {
-            fwrite(&count, sizeof count, 1, stdout);
-            fwrite(&prev, 1, 1, stdout);
+        char c;
+        char prev;
+        int count = 0;
+
+        while(1) {
+            char c = fgetc(fp);
+
+            if(feof(fp)) {
+                fwrite(&count, sizeof count, 1, stdout);
+                fwrite(&prev, 1, 1, stdout);
+                break;
+            }
+            
+            else if(c == prev) {
+                prev = c;
+                count++;
+                continue;
+            }
+
+            else if(count != 0) {
+                fwrite(&count, sizeof count, 1, stdout);
+                fwrite(&prev, 1, 1, stdout);
+            }
+            count = 1;
+            prev = c;
         }
-        count = 1;
-        prev = c;
+        fclose(fp);
     }
-    fclose(fp);
     return(0);
 }
